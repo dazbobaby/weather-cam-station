362f19d4e18ccf19fac4cc6f342a43a08f35d63c
dazbobaby <daz@daz-pi.com>
2015-11-16 08:40:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <meta charset="UTF-8">
 <meta name="description" content="Halifax UK weather station and webcam">
 <meta name="keywords" content="Halifax UK, webcam,weather cam,weather station,weather alert">
-<meta name="author" content="dazbobaby">
+<meta name="author" content="Darron Mullen">
 </head>
 <html>
 <body bgcolor="#242424">
@@ -27,17 +27,20 @@
 </html>
 
 <?php
-  $json_string = file_get_contents("http://api.wunderground.com/api/MY_API_CODE_GOES_HERE/geolookup/conditions/q/UK/Halifax.json");
+  $json_string = file_get_contents("http://api.wunderground.com/api/MY-API-KEY/geolookup/conditions/q/UK/Halifax.json");
   $parsed_json = json_decode($json_string);
   $location = $parsed_json->{'location'}->{'city'};
   $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
-  $wind_mph = $parsed_json->{'current_observation'}->{'wind_mph'};
-  $wind_gust_mph = $parsed_json->{'current_observation'}->{'wind_gust_mph'};
   $relative_humidity = $parsed_json->{'current_observation'}->{'relative_humidity'};
   $precip_today_string = $parsed_json->{current_observation}->{'precip_today_string'};
+  $windchill_c = $parsed_json->{current_observation}->{'windchill_c'};
+  $wind = $parsed_json->{current_observation}->{'wind_string'};
+  $pressure = $parsed_json->{current_observation}->{'pressure_mb'};
+
   
-  echo "Current temperature in ${location} is: ${temp_c}°c and current wind speed is: ${wind_mph}mph, with a gust of ${wind_gust_mph}mph<br />\n";
-  echo "Relative humidity is ${relative_humidity} and the rainfall is currently at ${precip_today_string} in the past 24 hours\n";
+  echo "Current temperature in ${location} is: ${temp_c}°c and the wind is ${wind} with a windchill that makes it feel like ${windchill_c}°c.<br />\n";
+  echo "Relative humidity is ${relative_humidity}, the pressure is ${pressure} and the rainfall is currently at ${precip_today_string} in the past 24 hours.\n";
+
 ?>
 
 
